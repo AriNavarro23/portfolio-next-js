9aea806cd27dccd12d2bf17316ea4564e504e028
Ariel Navarro <navarro.a.ariel@hotmail.com>
2023-03-28 00:09:13 -0300

    first

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Ariel Navarro | Front-end Developer</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+      </Head>
+    </>
+  )
+}
