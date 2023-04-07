ea6e02da3d7a4c39a3da5e40c04ab48b18534883
Ariel Navarro <navarro.a.ariel@hotmail.com>
2023-04-06 23:55:27 -0300

    first

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,14 +1,14 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { AiOutlineMenu, AiOutlineClose, AiOutlineMail } from "react-icons/ai";
-import { FaGithub, FaLinkedinIn  } from "react-icons/fa";
+import { FaGithub, FaLinkedinIn } from "react-icons/fa";
 import { BsFillPersonLinesFill } from "react-icons/bs";
 
 
 const Navbar = () => {
-    const [ nav, setNav ] = useState(false);
-    const [ shadow, setShadow ] = useState(false);
+    const [nav, setNav] = useState(false);
+    const [shadow, setShadow] = useState(false);
 
     const handleNav = () => {
         setNav(!nav);
@@ -16,63 +16,65 @@ const Navbar = () => {
 
     useEffect(() => {
         const handleShadow = () => {
-            if(window.scrollY >= 400) {
+            if (window.scrollY >= 400) {
                 setShadow(true);
             } else {
                 setShadow(false);
             }
         };
         window.addEventListener("scroll", handleShadow);
-    },[]);
+    }, []);
 
     return (
         <div className={shadow ? "fixed w-full h-20 shadow-xl z-[100] bg-[#5651e5] bg-opacity-40 transition-all duration-500" : "fixed w-full h-20 z-[100]"}>
             <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16 ">
-                <Image 
-                    src="" 
-                    alt="/" 
-                    width="125" 
-                    height="50" 
-                />
+                <Link href="/">
+                    <Image
+                        src=""
+                        alt="/"
+                        width="125"
+                        height="50"
+                    />
+                </Link>
                 <div>
                     <ul className="hidden md:flex">
                         <Link href="/">
                             <li className="ml-10 text-sm uppercase hover:border-b">Home</li>
                         </Link>
-                        <Link href="/">
+                        <Link href="/#about">
                             <li className="ml-10 text-sm uppercase hover:border-b">About</li>
                         </Link>
-                        <Link href="/">
+                        <Link href="/#skills">
                             <li className="ml-10 text-sm uppercase hover:border-b">Skill</li>
                         </Link>
-                        <Link href="/">
+                        <Link href="/#projects">
                             <li className="ml-10 text-sm uppercase hover:border-b">Projects</li>
                         </Link>
-                        <Link href="/">
+                        <Link href="/#contact">
                             <li className="ml-10 text-sm uppercase hover:border-b">Contact</li>
                         </Link>
                     </ul>
                     <div onClick={handleNav} className="md:hidden cursor-pointer">
-                        <AiOutlineMenu size={25}/>
+                        <AiOutlineMenu size={25} />
                     </div>
                 </div>
             </div>
 
-            <div className={nav ? "md:hidden fixed left-0 top-0 w-full h-screen bg-black/70" : "" }>
-                <div 
+            <div className={nav ? "md:hidden fixed left-0 top-0 w-full h-screen bg-black/70" : ""}>
+                <div
                     className={
-                        nav 
-                        ? "md:hidden fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500" 
-                        : "fixed left-[-100%] top-0 p-10 ease-in duration-500"
+                        nav
+                            ? "md:hidden fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500"
+                            : "fixed left-[-100%] top-0 p-10 ease-in duration-500"
                     }
                 >
                     <div>
                         <div className="flex w-full items-center justify-between">
-                            <Image 
-                            src='' 
-                            alt='/' 
-                            width='87' 
-                            height='35' 
+                            <Image
+                                src=''
+                                alt='/'
+                                width='87'
+                                height='35'
                             />
                             <div onClick={handleNav} className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer">
                                 <AiOutlineClose />
@@ -116,7 +118,7 @@ const Navbar = () => {
                                 </div>
                                 <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
                                     <BsFillPersonLinesFill />
-                                </div>      
+                                </div>
                             </div>
                         </div>
                     </div>
