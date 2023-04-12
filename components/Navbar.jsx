f14947c44b3ec0199a9eeb87904650129f28fe8f
Ariel Navarro <navarro.a.ariel@hotmail.com>
2023-04-11 23:55:33 -0300

    first

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -44,7 +44,14 @@ const Navbar = () => {
     }, []);
 
     return (
-        <div className={shadow ? "fixed w-full h-20 shadow-xl z-[100] bg-[#5651e5] bg-opacity-40 transition-all duration-500" : "fixed w-full h-20 z-[100]"}>
+        <div 
+            style={{backgroundColor: `${navBg}`}}
+            className={
+                shadow 
+                    ? "fixed w-full h-20 shadow-xl z-[100] bg-[#5651e5] bg-opacity-40 transition-all duration-500" 
+                    : "fixed w-full h-20 z-[100]"
+                }
+            >
             <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16 ">
                 <Link href="/">
                     <Image
