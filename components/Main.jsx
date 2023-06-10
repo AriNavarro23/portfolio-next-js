98ed8eb2283c428421347eb606dab5de2c216daf
AriNavarro23 <navarro.a.ariel@hotmail.com>
2023-06-10 15:34:18 -0300

    last

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,46 +1,52 @@
-import React from 'react'
-import { AiOutlineMail } from 'react-icons/ai'
-import { BsFillPersonLinesFill } from 'react-icons/bs'
-import { FaGithub, FaLinkedinIn } from 'react-icons/fa'
+import React from "react";
+import Link from "next/link";
+import { AiOutlineMail } from "react-icons/ai";
+import { BsFillPersonLinesFill } from "react-icons/bs";
+import { FaGithub, FaLinkedinIn } from "react-icons/fa";
 
 const Main = () => {
-    return (
-        <div id='main' className='w-full h-screen text-center py-16'>
-            <div className='max-w-[1240px] w-full mx-auto p-2 flex justify-center items-center flex-col py-16'>
-                <p className='uppercase text-sm tracking-widest text-gray-600'>
-                    Let's build something together
-                </p>
-                <h1 className='py-4 text-gray-700'>
-                    Hi, I'm <span className='text-[#5651e5]'>Ariel</span>
-                </h1>
-                <h1 className='py-2 text-gray-700'>
-                    A Front-End Web Developer
-                </h1>
-                <p className='py-4 text-gray-600 max-w-[70%]'>
-                    I'm a front-end webdeveloper specializing in building
-                    (and occasionally designing) exceptional websites, applications,
-                    and everything in between. Currently, I'm focused on building responsive
-                    front-end web applications while learning more about the back-end.
-                </p>
-                <div className='flex items-center justify-between max-w-[330px] py-4'>
-                    
-                    <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
-                        <FaLinkedinIn />
-                    </div>
-                    <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
-                        <FaGithub />
-                    </div>
-                    <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
-                        <AiOutlineMail />
-                    </div>
-                    <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
-                        <BsFillPersonLinesFill />
-                    </div>
-
-                </div>
-            </div>
+    const email = 'navarro.ari.dev@gmail.com';
+  return (
+    <div id="main" className="w-full h-screen text-center py-16">
+      <div className="max-w-[1240px] w-full mx-auto p-2 flex justify-center items-center flex-col py-16">
+        <p className="uppercase text-sm tracking-widest text-gray-600">
+          Let's build something together
+        </p>
+        <h1 className="py-4 text-gray-700">
+          Hi, I'm <span className="text-[#5651e5]">Ariel</span>
+        </h1>
+        <h1 className="py-2 text-gray-700">A Front-End Web Developer</h1>
+        <p className="py-4 text-gray-600 max-w-[70%]">
+          I'm a front-end webdeveloper specializing in building (and
+          occasionally designing) exceptional websites, applications, and
+          everything in between. Currently, I'm focused on building responsive
+          front-end web applications while learning more about the back-end.
+        </p>
+        <div className="flex items-center justify-between max-w-[330px] py-4">
+          <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+            <Link href="https://www.linkedin.com/in/ariel-navarro-42381a240">
+              <FaLinkedinIn />
+            </Link>
+          </div>
+          <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+            <Link href="https://github.com/AriNavarro23">
+              <FaGithub />
+            </Link>
+          </div>
+          <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+            <Link href={`mailto:${email}`}>
+              <AiOutlineMail />
+            </Link>
+          </div>
+          <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+            <Link href="https://www.linkedin.com/in/ariel-navarro-42381a240">
+              <BsFillPersonLinesFill />
+            </Link>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Main
+export default Main;
--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import Image from 'next/image';
-// import propertyImg from '../public/assets/projects/1.png';
 import Link from 'next/link';
+import Todolist from '../public/assets/projects/todolist.png';
+import Password from '../public/assets/projects/password.png';
+import Crud from '../public/assets/projects/crud.png';
+import Weather from '../public/assets/projects/weather.png';
 import ProjectItem from './ProjectItem';
 
 const Projects = () => {
@@ -12,23 +15,23 @@ const Projects = () => {
                 <h2 className='py-4'>What I've Built</h2>
                 <div className='grid md:grid-cols-2 gap-8'>
 
-                    <ProjectItem title='Project 1'
-                        backgroundImg=''
+                    <ProjectItem title='To do list App'
+                        backgroundImg={Todolist}
                         projectUrl='/project1'
                     />
 
-                    <ProjectItem title='Project 1'
-                        backgroundImg=''
+                    <ProjectItem title='Password Generator App'
+                        backgroundImg={Password}
                         projectUrl='/project1'
                     />
 
-                    <ProjectItem title='Project 1'
-                        backgroundImg=''
+                    <ProjectItem title='Crud App'
+                        backgroundImg={Crud}
                         projectUrl='/project1'
                     />
 
-                    <ProjectItem title='Project 1'
-                        backgroundImg=''
+                    <ProjectItem title='Weather App'
+                        backgroundImg={Weather}
                         projectUrl='/project1'
                     />
                 </div>
